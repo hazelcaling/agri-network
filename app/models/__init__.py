5551aa17853d5c284e27e5c04b3068a2cbcb15b4
hazelcaling <hazelcaling@live.com>
2024-10-08 22:10:07 -0700

    feat: create Message model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,4 +3,5 @@ from .user import User
 from .product import Product
 from .buyer_request import Buyer_Request
 from .image import Image
+from .message import Message
 from .db import environment, SCHEMA
--- app/models/message.py
+++ app/models/message.py
@@ -1,1 +1,32 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Message(db.Model):
+    __tablename__ = 'messages'
+
+    if environment == 'production':
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255), nullable=False)
+    sender_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    is_read = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.TIMESTAMP, default=db.func.current_timestamp())
+    updated_at = db.Column(db.TIMESTAMP, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'content': self.content,
+            'sender_id': self.sender_id,
+            'receiver_id': self.receiver_id,
+            'is_read': self.is_read,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+        }
+
+    # Relationships
+    farmer = db.relationship('User', back_populates='products')
+    images = db.relationship('Image', back_populates='product')
+    sender = db.relationship('User', foreign_keys=[sender_id], back_populates='sent_messages')
+    receiver = db.relationship('User', foreign_keys=[receiver_id], back_populates='received_messages')
